ec82242435dbbefb585ae67a53882cd004c02162
Eveliina Pakarinen <pevetiainen.ep@hotmail.com>
2017-09-15 23:35:21 +0300

    Add slash command support

--- app/controllers/motivations_controller.rb
+++ app/controllers/motivations_controller.rb
@@ -34,6 +34,28 @@ class MotivationsController < ApplicationController
     end
   end
 
+  def slash_motivate
+    respond_to do |format|
+      params_token = motivate_params[:token]
+      params_text = motivate_params[:text]
+      motivation_text = "Kyllä se siitä"
+
+      if !params_text.to_s.empty?
+          motivation_text = motivation_text + " " + params_text
+      end
+
+      if params_token == MATTERMOST_TOKEN || params_token == SECOND_MATTERMOST_TOKEN
+        matter_motivator = {response_type: "in_channel", username: "motivator", text: motivation_text + " :motivation-whale:" }
+        format.html
+        format.json { render json: matter_motivator }
+      else
+        format.html
+        format.json {render json: {}, status: :internal_server_error }
+      end
+
+    end
+  end
+
   private
   def motivate_params
     params.permit(:token, :user_name, :text)
--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,8 @@ Rails.application.routes.draw do
 
   post 'motivate', to: 'motivations#motivate'
 
+  post 'slash_motivate', to: 'motivations#slash_motivate'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
