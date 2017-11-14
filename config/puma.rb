aeeeb93776b792a128a21375b8fb5c5175ea04e4
Pakarinen Eveliina <Eveliina.Pakarinen@cinia.fi>
2017-11-14 19:19:31 +0200

    Add debug info

--- config/puma.rb
+++ config/puma.rb
@@ -17,8 +17,16 @@ environment ENV['RACK_ENV'] || 'development'
 on_worker_boot do
   # Valid on Rails up to 4.1 the initializer method of setting `pool` size
   ActiveSupport.on_load(:active_record) do
+    config1 = ActiveRecord::Base.configurations[Rails.env]
+    puts config1.to_yaml
+    puts '______________'
+    config2 = Rails.application.config.database_configuration[Rails.env]
+    puts config2.to_yaml
+    puts '______________'
     config = ActiveRecord::Base.configurations[Rails.env] ||
         Rails.application.config.database_configuration[Rails.env]
+    puts config.to_yaml
+    puts '______________'
     config['pool'] = ENV['MAX_THREADS'] || 5
     ActiveRecord::Base.establish_connection(config)
   end
